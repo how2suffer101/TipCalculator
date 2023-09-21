f2a0baccebf9ef4c33a366942a8c2bcc0b37452a
BT_1N3_17 <sample@bths.edu>
2023-09-21 08:58:12 -0400

    finished the Krusty Krab's menu

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -4,6 +4,31 @@ public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("")
+        System.out.println("Welcome to the Krusty Krab");
+        System.out.println();
+
+        System.out.println("-----------GALLEY GRUBS-----------");
+        System.out.println();
+        System.out.println("KRABBY PATTY.................$1.25");
+        System.out.println("DOUBLE KRABBY PATTY..........$2.00");
+        System.out.println("TRIPLE KRABBY PATTY..........$3.00");
+        System.out.println("KRABBY MEAL..................$3.50");
+        System.out.println("DOUBLE KRABBY MEAL...........$3.75");
+        System.out.println("TRIPLE KRABBY MEAL...........$4.00");
+        System.out.println("SALTY SEA DOG................$1.25");
+        System.out.println("FOOTLONG.....................$2.00");
+        System.out.println("SAILOR'S SURPRISE............$3.00");
+        System.out.println("GOLDEN LOAF..................$2.00");
+        System.out.println();
+        System.out.println("CORAL BITS S.................$1.00");
+        System.out.println("CORAL BITS M.................$1.25");
+        System.out.println("CORAL BITS L.................$1.50");
+        System.out.println();
+        System.out.println("KELP SHAKE...................$2.00");
+        System.out.println("KELP RINGS...................$1.50");
+        System.out.println();
+        System.out.println("SEAFOAM SODA S...............$1.00");
+        System.out.println("SEAFOAM SODA M...............$1.25");
+        System.out.println("SEAFOAM SODA L...............$1.50");
     }
 }
